e7bc5c02ccee3079036b315bec238c6acc33417f
Gill Chan <jiahao0408@gmail.com>
2022-02-04 16:31:42 +0800

    chore: tabs api

--- web/src/api/tabs.ts
+++ web/src/api/tabs.ts
@@ -1,4 +1,29 @@
 import { jsbridge } from 'gcjsbridge/src/invoker';
+enum TabStatus {
+
+}
+enum WindowType {
+
+}
+interface TabQueryInfo {
+  active?: boolean;
+  audible?: boolean;
+  autoDiscardable?: boolean;
+  currentWindow?: boolean;
+  discarded?: boolean;
+  groupId?: number;
+  highlighted?: boolean;
+  index?: number;
+  lastFocusedWindow?: boolean;
+  muted?: boolean;
+  pinned?: boolean;
+  status?: TabStatus;
+  title?: string;
+  url?: string | string[];
+  windowId?: number;
+  windowType?: WindowType;
+}
+
 export default class Tabs {
   async create(url: string) {
     return await jsbridge('runtime.tabs.create', url)
@@ -7,6 +32,19 @@ export default class Tabs {
     tabIds: number | number[],
     callback?: Function,
   ) {
-    jsbridge('runtime.tabs.remove',{tabIds}, callback);
+    jsbridge('runtime.tabs.remove', { tabIds }, callback);
+  }
+  query(
+    queryInfo: TabQueryInfo,
+    callback?: Function) {
+    jsbridge('runtime.tabs.query', { queryInfo }, callback);
+  }
+  sendMessage(
+    tabId: number,
+    message: any,
+    options?: { frameId: number | undefined },
+    callback?: Function,
+  ) {
+    jsbridge('runtime.tabs.query', { tabId, message, options }, callback);
   }
 }
